7d34abcaf80136d6017c01c5aabe69575ed3ac68
Ivan Šuvak Martinović <isuvak@gmail.com>
2022-05-15 01:53:23 -0400

    trying to add revalidation

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,6 +1,11 @@
 export default async function handler(req, res) {
     // Check for secret to confirm this is a valid request
     if (req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
+
+        console.log(req, 'req');
+        console.log(req.query, 'req.query');
+        console.log(req.query.secret, 'req.query.secret');
+
         return res.status(401).json({ message: 'Invalid token' })
     }
 
