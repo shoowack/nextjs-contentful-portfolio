083ca5ca09a403a82ac22a233ff5845ae84d6324
shoowack <isuvak@gmail.com>
2022-09-17 23:32:01 -0400

    removing unused code

--- components/front.page.js
+++ components/front.page.js
@@ -44,38 +44,3 @@ export default function FrontPage({
     </Link>
   );
 }
-
-
-
-
-{/* <>
-      <Link
-        href={link}
-        key={id}
-      >
-        <a
-          className={styles.section}
-          target={openInNewTab ? "_blank" : ""}
-        >
-          {title && subscript && (
-            <div className={styles["section-title"]}>
-              {title && <h1>{title}</h1>}
-              {subscript && <h3>{subscript}</h3>}
-            </div>
-          )}
-          {superscript && (
-            <div className={styles["section-description"]}>
-              <p>{superscript}</p>
-            </div>
-          )}
-          {url && <ContentfulImage
-            src={url}
-            layout="fill"
-            // height="100%"
-            // width="30%"
-            // layout="responsive"
-            className={styles["section-bg-img"]}
-          />}
-        </a>
-      </Link>
-    </> */}
