dc6aea698ec5d67ffa01f541a3eba4cc8c700c5b
Ivan <isuvak@gmail.com>
2023-01-23 08:31:34 -0500

    added hover effect to front page titles

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         'accent-7': '#333',
         success: '#0070f3',
         blue: colors.blue,
+        slate: colors.slate,
       },
       spacing: {
         28: '7rem',
@@ -37,6 +38,15 @@ module.exports = {
         small: '0 5px 10px rgba(0, 0, 0, 0.12)',
         medium: '0 8px 30px rgba(0, 0, 0, 0.12)',
       },
+      keyframes: {
+        'shine': {
+          '0%': { backgroundPosition: '-100%' },
+          '100%': { backgroundPosition: '100%' },
+        }
+      },
+      animation: {
+        shine: 'shine 2s ease-in-out',
+      }
     },
   },
   plugins: [],
