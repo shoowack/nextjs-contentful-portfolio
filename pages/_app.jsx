064801fa22bf9cc3a55d33ffda14f6c2d5f69d33
Ivan <isuvak@gmail.com>
2023-01-25 15:34:15 -0500

    added breakpoint indicator to development env

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -8,6 +8,17 @@ import '../styles/index.css';
 const App = ({ Component, pageProps }) => {
   return (
     <>
+      {process.env.NODE_ENV === 'development' && (
+        <div className="fixed z-50 p-2 text-xs font-bold text-black">
+          <span className="rounded bg-yellow-400 p-1 px-1 sm:hidden">XS</span>
+          <span className="hidden rounded bg-yellow-400 px-1 sm:inline-block md:hidden">SM</span>
+          <span className="hidden rounded bg-yellow-400 px-1 sm:hidden md:inline-block lg:hidden">
+            MD
+          </span>
+          <span className="hidden rounded bg-yellow-400 px-1 lg:inline-block xl:hidden">LG</span>
+          <span className="hidden rounded bg-yellow-400 px-1 xl:inline-block">XL</span>
+        </div>
+      )}
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </>
