62e406495066c7181a66a7676a58aad19bd9a020
Ivan <isuvak@gmail.com>
2023-01-26 17:41:09 -0500

    BreakpointIndicator component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,21 +4,12 @@ import '../styles/inner-page.scss';
 import '../styles/swiper.scss';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
 import '../styles/index.css';
+import BreakpointIndicator from '@components/BreakpointIndicator';
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
-      {process.env.NODE_ENV === 'development' && (
-        <div className="fixed top-1 left-0 z-[1090] p-2 text-xs font-bold text-black sm:left-1 sm:top-2 md:left-2 md:top-2">
-          <span className="rounded bg-yellow-400 p-1 px-1 sm:hidden">XS</span>
-          <span className="hidden rounded bg-yellow-400 px-1 sm:inline-block md:hidden">SM</span>
-          <span className="hidden rounded bg-yellow-400 px-1 sm:hidden md:inline-block lg:hidden">
-            MD
-          </span>
-          <span className="hidden rounded bg-yellow-400 px-1 lg:inline-block xl:hidden">LG</span>
-          <span className="hidden rounded bg-yellow-400 px-1 xl:inline-block">XL</span>
-        </div>
-      )}
+      <BreakpointIndicator />
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </>
