d07b087a6ae78cdd74d9e2bba4ab1d45fa0ca22a
Ivan <isuvak@gmail.com>
2023-02-15 21:19:17 -0500

    trying out new footer design

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,6 +5,31 @@ import Container from '@components/Container';
 
 import axios from 'axios';
 
+export const syntaxHighlight = (json) => {
+  if (typeof json !== 'string') {
+    json = JSON.stringify(json, undefined, 2);
+  }
+  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  return json.replace(
+    /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+    function (match) {
+      let cls = 'json-number';
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
+          cls = 'json-key';
+        } else {
+          cls = 'json-string';
+        }
+      } else if (/true|false/.test(match)) {
+        cls = 'json-boolean';
+      } else if (/null/.test(match)) {
+        cls = 'json-null';
+      }
+      return `<span class="${cls}">${match}</span>`;
+    },
+  );
+};
+
 export default function Footer() {
   const [isLoading, setIsLoading] = useState(false);
   const [sentState, setSentState] = useState(false);
@@ -67,7 +92,85 @@ export default function Footer() {
   const { email, message } = form;
 
   return (
-    <div className="fixed bottom-0 -z-10 w-full bg-[#e9eaed] dark:bg-[#2c2d30] text-[#333] dark:text-white md:py-10">
+    <div className="footer fixed bottom-0 -z-10 w-full  text-[#333] dark:text-white md:pt-36 md:pb-10">
+      <pre
+        className="text-xs absolute -top-1/2 -z-10 pointer-events-none select-none opacity-30"
+        dangerouslySetInnerHTML={{
+          __html: syntaxHighlight({
+            id: '63ed59baa00062440f867a4d',
+            index: 0,
+            guid: '217ba6f1-fe1f-4545-88e9-4416cffa16db',
+            isActive: false,
+            balance: '$3,597.41',
+            picture: 'http://placehold.it/32x32',
+            age: 33,
+            eyeColor: 'green',
+            name: 'Kristie Frye',
+            gender: 'female',
+            company: 'ROUGHIES',
+            address: '500 Bliss Terrace, Lisco, Arizona, 3870',
+            about: 'Quis veniam culpa culpa in fugiat culpa consequat tempor nostrud aliqua',
+            registered: '2014-08-07T06:00:56 +04:00',
+            latitude: 71.520365,
+            longitude: 40.292762,
+            tags: ['reprehenderit', 'nostrud', 'minim', 'aliqua', 'sunt', 'qui', 'dolor'],
+            friends: [
+              {
+                id: 0,
+                name: 'Jeannine Sharpe',
+              },
+              {
+                id: 1,
+                name: 'Boone Valenzuela',
+              },
+              {
+                id: 2,
+                name: 'Mooney Sherman',
+              },
+            ],
+            greeting: 'Hello, Kristie Frye! You have 1 unread messages.',
+            favoriteFruit: 'strawberry',
+          }),
+        }}
+      />
+      <pre
+        className="left-1/3 text-sm absolute bottom-0 -z-10 pointer-events-none select-none opacity-10"
+        dangerouslySetInnerHTML={{
+          __html: syntaxHighlight({
+            registered: '2021-12-18T01:21:55 +05:00',
+            latitude: 28.660751,
+            longitude: -159.78149,
+            tags: ['enim', 'fugiat', 'esse', 'ad', 'ullamco', 'Lorem', 'elit'],
+            friends: [
+              {
+                id: 0,
+                name: 'Paula Hensley',
+              },
+              {
+                id: 1,
+                name: 'Aisha Lester',
+              },
+              {
+                id: 2,
+                name: 'Lina Ware',
+              },
+            ],
+            greeting: 'Hello, undefined! You have 8 unread messages.',
+            favoriteFruit: 'strawberry',
+          }),
+        }}
+      />
+      <pre
+        className="text-md right-0 absolute bottom-0 -z-10 pointer-events-none select-none opacity-20"
+        dangerouslySetInnerHTML={{
+          __html: syntaxHighlight({
+            registered: '2020-01-15T03:34:19 +05:00',
+            latitude: -11.968522,
+            longitude: -6.819732,
+            tags: ['mollit', 'reprehenderit', 'esse', 'dolor', 'proident', 'minim', 'nostrud'],
+          }),
+        }}
+      />
       <Container>
         <h2 className="text-nowrap flex justify-center text-3xl font-black leading-[78px] md:text-[60px]">
           Contact
