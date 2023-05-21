b148336c98dc9382d8450b5ff17843c8f9cab3ca
Ivan <isuvak@gmail.com>
2023-05-21 14:54:44 -0400

    adjusting carousel page indicators styles for iPads, removing unused stuff

--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -1,11 +1,11 @@
+import Footer from '@components/Footer';
 import Navigation from '@components/Navigation';
 import Section from '@components/Section';
+import Layout from '@components/layout';
 import { getHeaderLinks } from '@lib/api';
 import { fetchEntries } from '@lib/fetchEntries';
-import Footer from '@components/Footer';
-import { Provider } from 'react-wrap-balancer';
 import useWindowDimensions from '@lib/windowSize';
-import Layout from '@components/layout';
+import { Provider } from 'react-wrap-balancer';
 
 export default function InnerPage({ entries, headerItems }) {
   const { width } = useWindowDimensions();
@@ -15,14 +15,14 @@ export default function InnerPage({ entries, headerItems }) {
       <div className="z-[2] mb-[360px] min-h-full overflow-hidden shadow-[0_10px_30px_-5px_rgba(0,0,0,.2)] dark:shadow-[0_10px_30px_rgba(0,0,0,.5)] sm:mb-[300px] md:mb-[400px] md:shadow-[0_10px_60px_-10px_rgba(0,0,0,.2)] md:dark:shadow-[0_10px_60px_rgba(0,0,0,.5)]">
         <Navigation headerItems={headerItems} />
         {/* filter sections by environment tags (production, development) */}
-        {entries[0].sections.map(
-          (entry, i) =>
-            entry.metadata.tags.some((tags) => tags.sys.id === process.env.NODE_ENV) && (
-              <Provider key={`provider-${entry.sys.id}`}>
+        <Provider>
+          {entries[0].sections.map(
+            (entry, i) =>
+              entry.metadata.tags.some((tags) => tags.sys.id === process.env.NODE_ENV) && (
                 <Section windowWidth={width} {...entry.fields} i={i} />
-              </Provider>
-            ),
-        )}
+              ),
+          )}
+        </Provider>
         <Footer />
       </div>
     </Layout>
